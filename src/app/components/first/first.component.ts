68caba4071412432ed72768d391539fe03596334
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-13 16:16:54 +0100

    exemples binding

---
+++ src/app/components/first/first.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css']
+})
+export class FirstComponent {
+  //State
+  name = "Angélique";
+  isHidden = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.name = 'Fabrice'
+    }, 2000);
+
+    // setInterval(() => {
+    //   this.isHidden = !this.isHidden;
+    // },1000)
+  }
+  //Comportement
+  showHide() {
+    this.isHidden = !this.isHidden;
+  }
+}
---
+++ src/app/components/second/second.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second',
+  template: `
+    <div class="alert alert-dark">
+      second Component
+    </div>
+  `,
+  styles: [``]
+})
+export class SecondComponent {
+
+}
