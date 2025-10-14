4a1ff49ecaa3ece714f936cec90883a3c577fe85
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-14 16:33:16 +0100

    todo exercice

--- src/app/todo/todo/todo.component.ts
+++ src/app/todo/todo/todo.component.ts
@@ -1,11 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { TodoService } from "../service/todo.service";
+import { Todo } from "../model/todo";
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css'],
-
 })
 export class TodoComponent {
-  constructor() {}
+  todoService = inject(TodoService);
+  todos = this.todoService.getTodos();
+  todo = new Todo();
+
+  addTodo() {
+    this.todoService.addTodo(this.todo);
+    this.todo = new Todo();
+  }
+  deleteTodo(todo: Todo) {
+    this.todoService.deleteTodo(todo);
+  }
 }
