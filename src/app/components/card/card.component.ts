b8099e0129d4545862a6d74ed1b054043e27d75e
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-14 09:27:42 +0100

    correction exo card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-
+  firstname = 'aymen';
+  name = 'sellaouti';
+  job = 'Enseignant';
+  path = 'rotating_card_profile3.png';
 }
