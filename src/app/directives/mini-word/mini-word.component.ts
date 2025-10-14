7cad5731617312dd0d729a1249835aaaab387e4c
aymensellaouti <aymen.sellaouti@gmail.com>
2025-10-14 13:23:17 +0100

    correction exo mini word

--- src/app/directives/mini-word/mini-word.component.ts
+++ src/app/directives/mini-word/mini-word.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./mini-word.component.css']
 })
 export class MiniWordComponent {
-
+  color = 'red';
+  fontFamily = 'garamond';
+  fontSize = 75;
 }
